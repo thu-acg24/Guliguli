5107f07b7cc271b823fa524a9de5384e8fb4b42b
lcw <ez_lcw@foxmail.com>
2025-06-29 18:27:10 +0800

    Add 'Styles' folder

--- client/src/renderer.tsx
+++ client/src/renderer.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import { HashRouter, Route, Routes } from 'react-router-dom'
-// import LoginPage, {loginPagePath} from "Pages/LoginPage";
 import MainPage, { mainPagePath } from "Pages/MainPage";
-// import RegisterPage, {registerPagePath} from "Pages/RegisterPage";
-// import BookPage, {bookPagePath} from "Pages/BookPage";
+import './Styles/index.css';
+// import HomePage, { homePagePath } from 'Pages/HomePage';
+// import MessagePage, { messagePagePath } from 'Pages/MessagePage';
+// import ManagementPage, { managementPagePath } from 'Pages/ManagementPage';
+// import VideoPage, { videoPagePath } from 'Pages/VideoPage';
+// import SearchPage, { searchPagePath } from 'Pages/SearchPage';
+// import MemberPage, { memberPagePath } from 'Pages/MemberPage';
+
 
 const Layout = () => {
     return (
         <HashRouter>Main
             <Routes>
                 <Route path="/" element={<MainPage />} />
-                {/* <Route path={registerPagePath} element={<RegisterPage />} /> */}
-                {/* <Route path={loginPagePath} element={<LoginPage />} />
-                <Route path={bookPagePath} element={<BookPage />} /> */}
                 <Route path={mainPagePath} element={<MainPage />} />
+                {/* <Route path={homePagePath} element={<HomePage />} />
+                <Route path={messagePagePath} element={<MessagePage />} />
+                <Route path={managementPagePath} element={<ManagementPage />} />
+                <Route path={videoPagePath} element={<VideoPage />} />
+                <Route path={searchPagePath} element={<SearchPage />} />
+                <Route path={memberPagePath} element={<MemberPage />} /> */}
             </Routes>
         </HashRouter>
     )
