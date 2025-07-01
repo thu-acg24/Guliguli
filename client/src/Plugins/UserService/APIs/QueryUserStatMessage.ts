521583797654f29c658d738d2da5f0590f7acde9
Nikrot <1223826357@qq.com>
2025-07-01 15:49:35 +0800

    Update UserStat Client

--- client/src/Plugins/UserService/APIs/QueryUserStatMessage.ts
+++ client/src/Plugins/UserService/APIs/QueryUserStatMessage.ts
@@ -2,7 +2,7 @@
  * QueryUserStatMessage
  * desc: 根据用户ID查询用户表，返回用户的统计数据。
  * @param userID: Int (用户唯一标识，表示需要查询的用户ID。)
- * @return user: UserStat (用户统计数据，包括视频数量、追随者数量、粉丝数量等。)
+ * @return user: UserStat (用户统计数据，追随者数量、粉丝数量等。)
  */
 import { TongWenMessage } from 'Plugins/TongWenAPI/TongWenMessage'
 
--- client/src/Plugins/UserService/Objects/UserStat.ts
+++ client/src/Plugins/UserService/Objects/UserStat.ts
@@ -3,8 +3,6 @@
  * desc: 用户的统计信息，包括粉丝、关注、视频数和收藏夹视频数
  * @param followerCount: Int (粉丝数量)
  * @param followingCount: Int (关注的人数)
- * @param videoCount: Int (上传的视频数量)
- * @param favoriteVideoCount: Int (收藏夹中的视频数量)
  */
 import { Serializable } from 'Plugins/CommonUtils/Send/Serializable'
 
@@ -15,8 +13,6 @@ export class UserStat extends Serializable {
     constructor(
         public  followerCount: number,
         public  followingCount: number,
-        public  videoCount: number,
-        public  favoriteVideoCount: number
     ) {
         super()
     }
