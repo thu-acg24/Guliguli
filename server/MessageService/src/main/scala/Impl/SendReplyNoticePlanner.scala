57a90bd8a0266644ab4f98940972b03415e9e48c
Nikrot <1223826357@qq.com>
2025-07-02 22:00:20 +0800

    Add SendReplyNotice

---
+++ server/MessageService/src/main/scala/Impl/SendReplyNoticePlanner.scala
@@ -0,0 +1,84 @@
+package Impl
+
+import APIs.CommentService.QueryCommentByIDMessage
+import APIs.UserService.{GetUIDByTokenMessage, QueryUserInfoMessage, QueryUserRoleMessage}
+import Common.API.{PlanContext, Planner}
+import Common.APIException.InvalidInputException
+import Common.DBAPI.*
+import Common.Object.SqlParameter
+import Common.Serialize.CustomColumnTypes.{decodeDateTime, encodeDateTime}
+import Common.ServiceUtils.schemaName
+import Objects.UserService.{UserInfo, UserRole}
+import cats.effect.IO
+import cats.implicits.*
+import io.circe.*
+import io.circe.generic.auto.*
+import io.circe.syntax.*
+import org.joda.time.DateTime
+import org.slf4j.LoggerFactory
+
+case class SendReplyNoticePlanner(
+    token: String,
+    commentID: Int,
+    override val planContext: PlanContext
+) extends Planner[Unit] {
+  private val logger = LoggerFactory.getLogger(this.getClass.getSimpleName + "_" + planContext.traceID.id)
+
+  override def plan(using PlanContext): IO[Unit] = {
+    for {
+      // Step 1: 验证调用者Token的合法性
+      _ <- IO(logger.info(s"开始验证调用者Token: ${token}"))
+      userID <- GetUIDByTokenMessage(token).send
+      _ <- IO(logger.info(s"验证成功，用户: ${userID}"))
+      // Step 2: 检查Comment是否存在
+      comment <- QueryCommentByIDMessage(commentID).send
+      replyID <- IO {
+        comment.replyToID match {
+          case Some(id) => id
+          case None => throw InvalidInputException("评论不存在被回复者")
+        }
+      }
+      _ <- if comment.authorID == userID then IO.unit else IO.raiseError(InvalidInputException("用户不是评论发布者"))
+      replyComment <- QueryCommentByIDMessage(replyID).send
+      receiverID = replyComment.authorID
+      // Step 3: 检测评论是否存在
+      _ <- IO(logger.info("检查评论是否存在"))
+      _ <- checkRecord(commentID)
+      // Step 3: 插入数据库
+      _ <- IO(logger.info("消息构造成功，开始插入数据库"))
+      timestamp <- IO(DateTime.now())
+      constructedMessage <- insertRecord(userID, receiverID, comment.content, commentID, timestamp)
+    } yield()
+  }
+
+  private def checkRecord(commentID: Int)(using PlanContext): IO[Boolean] = {
+    val sql =
+      s"""
+         SELECT COUNT(*) from ${schemaName}.message_table WHERE comment_id = ?;
+       """
+    for {
+      count <- readDBInt(sql, List(SqlParameter("Int", commentID.toString)))
+    } yield count > 0
+  }
+
+  private def insertRecord(senderID: Int, receiverID: Int, content: String, commentID: Int, timestamp: DateTime)
+                          (using PlanContext): IO[Unit] = {
+    val sql =
+      s"""
+         INSERT INTO ${schemaName}.message_table
+         (sender_id, receiver_id, content, comment_id, send_time)
+         VALUES (?, ?, ?, ?, ?);
+       """
+
+    writeDB(
+      sql,
+      List(
+        SqlParameter("Int", senderID.toString),
+        SqlParameter("Int", receiverID.toString),
+        SqlParameter("String", content),
+        SqlParameter("Int", commentID.toString),
+        SqlParameter("DateTime", timestamp.getMillis.toString),
+      )
+    ).as(())
+  }
+}
