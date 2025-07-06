5ad357f12371a7d9d2a078fc39056c205a0af75b
lcw <ez_lcw@foxmail.com>
2025-07-06 23:11:36 +0800

    处理了将bio引入UserInfo造成的影响

--- client/src/Pages/HomePage/SettingsTab.tsx
+++ client/src/Pages/HomePage/SettingsTab.tsx
@@ -1,8 +1,6 @@
 // src/Pages/HomePage/SettingsTab.tsx
 import React from "react";
 import { useOutletContext } from "react-router-dom";
-import { useUserToken } from "Globals/GlobalStore";
-import { useRefreshUserInfo } from "Globals/UserHooks";
 import UserInfoForm from "./SettingsTab/UserInfoForm";
 import PasswordForm from "./SettingsTab/PasswordForm";
 import "./HomePage.css";
@@ -11,20 +9,14 @@ const SettingsTab: React.FC<{ userInfo?: any }> = (props) => {
     const outlet = useOutletContext<{ userInfo: any, refreshUserInfo?: () => void }>();
     const userInfo = props.userInfo ?? outlet?.userInfo;
     const refreshHomePageUserInfo = outlet?.refreshUserInfo;
-    const userToken = useUserToken();
-    const refreshUserInfo = useRefreshUserInfo();
 
     return (
         <div className="home-settings-tab">
             <UserInfoForm
                 userInfo={userInfo}
-                userToken={userToken}
-                refreshUserInfo={refreshUserInfo}
                 refreshHomePageUserInfo={refreshHomePageUserInfo}
             />
-            <PasswordForm
-                userToken={userToken}
-            />
+            <PasswordForm />
         </div>
     );
 };
